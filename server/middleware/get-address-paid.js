f41f778b7f2540740fc875291fbc2441f2a17c14
mango <8378656+MangoSalad@users.noreply.github.com>
2018-01-20 22:20:21 -0500

    Fixed isPaid

--- server/middleware/get-address-paid.js
+++ server/middleware/get-address-paid.js
@@ -3,8 +3,9 @@ const exec = require('child_process').exec;
 const getAddressPaid = () => {
 
   var getIsPaid = exec("$(pwd)'/src/wallet' 2", function(err, data) {  
-    var isPaid = data.toString();                       
-    console.log("isPaid:", isPaid);
+    var isPaid = data.toString();    
+    return isPaid;                   
+    //console.log("isPaid:", isPaid);
   })
 
   return async ctx => {
