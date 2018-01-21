1207605153d3807c69bd4a9c91bb36efe2724e03
Pavel Volek <me@pavelvolek.cz>
2018-01-21 05:49:19 +0100

    Fetch btc price

--- server/middleware/get-price.js
+++ server/middleware/get-price.js
@@ -1,26 +1,34 @@
 const exec = require('child-process-promise').exec;
+const fetch = require("node-fetch");
 
 const getPrice = () => {
 
   return async ctx => {
     var address = await exec("$(pwd)'/src/wallet' 1").then ((address) => {
-      
+
       console.log(address.stdout);
-      
+
       address = address.stdout.toString();
       address = address.substring(0,address.length - 1)
 
       return address;
     });
 
+    const btcValue = fetch("https://api.coinbase.com/v2/prices/spot?currency=USD")
+      .then(response => response.json().then(json => {
+        return json.data.amount
+      }))
+
+    const btcPrice = 1 / btcValue;
+
     ctx.status = 200;
     ctx.body = {
       address,
+      btcPrice,
       usdPrice: 1,
-      btcPrice: 0.0001,
     };
   };
 }
-  
+
 
 module.exports = getPrice;
