6c281708e0bdc43bc48dc6de9d8ca8c1520bdbdc
mango <8378656+MangoSalad@users.noreply.github.com>
2018-01-20 21:09:02 -0500

    Added address and ispaid

--- server/middleware/get-address-paid.js
+++ server/middleware/get-address-paid.js
@@ -2,7 +2,7 @@ const exec = require('child_process').exec;
 
 const getAddressPaid = () => {
 
-  exec("$(pwd)'/src/wallet' 2", function(err, data) {  
+  var getIsPaid = exec("$(pwd)'/src/wallet' 2", function(err, data) {  
     var isPaid = data.toString();                       
     console.log("isPaid:", isPaid);
   })
@@ -10,7 +10,7 @@ const getAddressPaid = () => {
   return async ctx => {
     ctx.status = 200;
     ctx.body = {
-      paid: false,
+      paid: isPaid(),
     };
   };
 };
--- server/middleware/get-price.js
+++ server/middleware/get-price.js
@@ -2,16 +2,15 @@ const exec = require('child_process').exec;
 
 const getPrice = () => {
   //get wallet address
-  exec("$(pwd)'/src/wallet' 1", function(err, data) {  
+  var getAddress = exec("$(pwd)'/src/wallet' 1", (err, data) => {  
     var address = data.toString();                       
     console.log("Address:", address);
-  })
+  });
   
-  //TODO, async wallet address func
   return async ctx => {
     ctx.status = 200;
     ctx.body = {
-      address: address,
+      address: getAddress(),
       btcPrice: 0.0001,
     };
   };
